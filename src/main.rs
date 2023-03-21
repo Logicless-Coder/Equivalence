73799f13af12c53d116eeae775c32625adc71501
Abhishek Rathore <abhirathore565@gmail.com>
2023-03-21 10:22:06 +0530

    Added a counter along with the equivalent rules

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::BufRead};
+use std::fs;
 use regex::Regex;
 use itertools::Itertools;
 
@@ -12,7 +12,6 @@ fn main() {
     let theta = &cap["theta"];
     let table = &cap["table"];
     println!("{theta} {table} {cols}");
-    // let conditions = Regex::new(r"([a-zA-Z0-9_<>=!]+)( (and|or) ([a-zA-Z0-9_<>=!]+))*").unwrap();
 
     let compound = query.replace_all(&expression, "$theta");
     let mut conditions = compound.split(" ").map(|x| x.trim()).collect::<Vec<_>>();
@@ -20,15 +19,15 @@ fn main() {
 
     let columns = cols.split(',').map(|x| x.trim()).collect::<Vec<_>>();
 
-    // let conds: Vec<String> = conditions.captures_iter(&after).map(|x| String::from(&x[0])).collect::<Vec<String>>();
-    // println!("{:?}", conds);
-
+    let mut counter = 1;
     for condition in conditions.iter().permutations(conditions.len()) {
         for column in columns.iter().permutations(columns.len()) {
-            // println!("{:?} {:?} {:?}", column, condition, table);
             let column_string = column.iter().join(", ");
             let condition_string = condition.iter().join(" and ");
-            println!("$[{}](@[{}]({}))", column_string, condition_string, table);
+
+            println!("{:2}: $[{}](@[{}]({}))", counter, column_string, condition_string, table);
+
+            counter += 1;
         }
     }
 }
